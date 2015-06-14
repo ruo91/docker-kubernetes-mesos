a87d969fffd2f38756965be4bd00f3e56d03d32a
Yongbok Kim <ruo91@yongbok.net>
2015-06-14 20:08:14 +0900

    fix - Add client-urls options of conf/local/00_etcd-cluster.sh

--- conf/local/00_etcd-cluster.sh
+++ conf/local/00_etcd-cluster.sh
@@ -10,4 +10,6 @@ LOCAL_IP="`ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1 }'`
 # Solutions: --listen-client-urls http://0.0.0.0:4001
 etcd \
 --data-dir "/tmp/etcd" \
+--listen-client-urls http://0.0.0.0:4001 \
+--advertise-client-urls http://0.0.0.0:4001 \
 > /tmp/etcd-cluster.log 2>&1 &
