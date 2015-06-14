1092910428a35106bf614d67d2650ea0a21aafc5
Yongbok Kim <ruo91@yongbok.net>
2015-06-14 18:04:51 +0900

    Remove the variable FRAMEWORK_NAME and options for conf/local/02_scheduler.sh

--- conf/local/02_scheduler.sh
+++ conf/local/02_scheduler.sh
@@ -4,9 +4,7 @@ PATH=$PATH:$K8SM
 
 ETCD_SERVER="http://127.0.0.1:4001"
 ZK_SERVER="zk://127.0.0.1:2181"
-
 MESOS_USER="root"
-FRAMEWORK_NAME="kubernetes-mesos"
 
 km scheduler \
 --v=2 \
@@ -15,5 +13,4 @@ km scheduler \
 --mesos_user=$MESOS_USER \
 --mesos_master=$ZK_SERVER \
 --etcd_servers=$ETCD_SERVER \
---framework_name=$FRAMEWORK_NAME \
 > /tmp/scheduler.log 2>&1 &
