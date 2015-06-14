57d6147e342f72617c27221f9d2eacd6c0b29edf
Yongbok Kim <ruo91@yongbok.net>
2015-06-14 20:10:10 +0900

    fix - Change the options value of conf/local/02_scheduler.sh

--- conf/local/02_scheduler.sh
+++ conf/local/02_scheduler.sh
@@ -3,12 +3,12 @@ K8SM=/opt/k8sm
 PATH=$PATH:$K8SM
 
 ETCD_SERVER="http://127.0.0.1:4001"
-ZK_SERVER="zk://127.0.0.1:2181"
+ZK_SERVER="127.0.0.1:5050"
 MESOS_USER="root"
 
 km scheduler \
 --v=2 \
---address=127.0.0.1 \
+--address=0.0.0.0 \
 --api_servers=127.0.0.1:8080 \
 --mesos_user=$MESOS_USER \
 --mesos_master=$ZK_SERVER \
