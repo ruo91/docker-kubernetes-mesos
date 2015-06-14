a8d78da6a667950ff4a758e0acac8b87acecc79d
Yongbok Kim <ruo91@yongbok.net>
2015-06-14 17:30:15 +0900

    fix - remove the client-urls options of conf/local/00_etcd-cluster.sh

--- conf/local/00_etcd-cluster.sh
+++ conf/local/00_etcd-cluster.sh
@@ -10,6 +10,4 @@ LOCAL_IP="`ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1 }'`
 # Solutions: --listen-client-urls http://0.0.0.0:4001
 etcd \
 --data-dir "/tmp/etcd" \
---listen-client-urls "http://$LOCAL_IP:4001" \
---advertise-client-urls "http://$LOCAL_IP:4001" \
 > /tmp/etcd-cluster.log 2>&1 &
